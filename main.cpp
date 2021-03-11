3af047b6c7bc0a10722287aa014fe4eb2ca2c947
Luis Delgado <62965678+Tiquillo@users.noreply.github.com>
2021-03-10 21:10:10 -0600

    Added interactive menu

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 #include "Node.h"
 #include "List.h"
 #include "Collector.h"
@@ -32,6 +33,62 @@ int main() {
 
     lista.PrintList();
     Collector::GetInstance()->PrintCollectorList();
+
+
+    // menú
+
+    char seleccion = '1';
+
+    while (seleccion != '0') {
+        cout << endl << "Seleccione la opci\xa2n que desea hacer a continuaci\xa2n:" << endl;
+        cout << "Salir 0" << endl;
+        cout << "A" << "\xa4" << "adir elemento 1" << endl;
+        cout << "Remover elemento 2" << endl;
+        cout << "Mostrar listas 3" << endl << endl;
+
+        cin >> seleccion;
+        int numero;
+        try {
+            switch ((int)seleccion) {
+                case '0':
+                    cout << "Gracias, que tenga lindo d" << "\xa1" << "a." << endl;
+                    break;
+
+                case '1':
+
+                    cout << "Escriba el n\xa3mero que desea a" << "\xa4" << "adir: ";
+                    try {
+                        cin >> numero;
+                        lista.AddFront(numero);
+                    } catch(exception e) {
+                        cout << "Lo lamento, el dato introducido no es v\xa0lido, volver\xa0 al men\xa3 principal." << endl << endl;
+                    }
+                    break;
+
+                case '2':
+                    cout << "Introduzca el \xa1ndice del elemento que desea eliminar." << endl;
+                    cin >> numero;
+                    lista.Remove(numero);
+                    break;
+
+                case '3':
+                    lista.PrintList();
+                    Collector::GetInstance()->PrintCollectorList();
+                    break;
+
+                default:
+                    cout << "Por favor, seleccione un n\xa3mero entre los indicados anteriormente." << endl;
+                    break;
+            }
+        } catch (exception e) {
+            cout << "Lo lamento, el dato introducido no es v\xa0lido, int\x82ntelo de nuevo." << endl << endl;
+        }
+
+
+    }
+
+
+    getch();
 
     return 0;
 }
